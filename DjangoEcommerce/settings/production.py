e607ff7f79a0a7f7bab0f970e4b1b4e182cc8302
snipher <sniphermarube@gmail.com>
2024-08-19 10:21:47 +0300

    fixing sessions data curruption

--- DjangoEcommerce/settings/production.py
+++ DjangoEcommerce/settings/production.py
@@ -1,12 +1,10 @@
-# production.py
 from .base import *
 
-
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
-# Configure your production database 
+# Database settings
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -19,17 +17,17 @@ DATABASES = {
     }
 }
 
-
+# Session settings
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 SECURE_HSTS_SECONDS = 60
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_HSTS_PRELOAD = True
 
 DOMAIN = "https://django-ecommerce-gamma.vercel.app/"
-
