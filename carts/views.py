f370662cb6417181e8548e4096252e2418994f04
snipher <sniphermarube@gmail.com>
2024-08-20 22:23:47 +0300

    adding products to cart

--- carts/views.py
+++ carts/views.py
@@ -1,31 +1,35 @@
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-
 from products.models import Product
 from .models import Cart, CartItem
 
 def _cart_id(request):
-    cart = request.session.session_key
-    if not cart:
-        cart = request.session.create()
-    return cart
+    """
+    Retrieve or create a unique cart ID for the user's session.
 
+    :param request: The HTTP request object.
+    :return: A unique cart ID.
+    """
+    if not request.session.session_key:
+        request.session.save()
+    return request.session.session_key
 
 def add_cart(request, product_id):
-    product = get_object_or_404(Product, id=product_id)  # Safely get the product or return a 404
+    product = get_object_or_404(Product, id=product_id)
     current_user = request.user
-    cart_item = None
 
     if current_user.is_authenticated:
-        # Get or create a cart item for the authenticated user
+        # For authenticated users, use CartItem associated with the user
         cart_item, created = CartItem.objects.get_or_create(
             product=product,
             user=current_user,
             defaults={'quantity': 1}
         )
     else:
-        # Get or create the cart and cart item for the anonymous user
-        cart, _ = Cart.objects.get_or_create(cart_id=_cart_id(request))
+        # For anonymous users, use Cart associated with the session
+        cart, created = Cart.objects.get_or_create(
+            cart_id=_cart_id(request)
+        )
         cart_item, created = CartItem.objects.get_or_create(
             product=product,
             cart=cart,
@@ -33,7 +37,7 @@ def add_cart(request, product_id):
         )
 
     if not created:
-        # If the item is already in the cart, increase the quantity
+        # If item already exists, update the quantity
         cart_item.quantity += 1
         cart_item.save()
         messages.info(request, f'Updated {product.product_name} quantity to {cart_item.quantity}.')
@@ -42,10 +46,16 @@ def add_cart(request, product_id):
 
     return redirect('cart')
 
-
-
 def cart(request):
-    return render(request, 'carts/checkout.html')
+    # Retrieve cart items for authenticated users or session-based cart for anonymous users
+    if request.user.is_authenticated:
+        cart_items = CartItem.objects.filter(user=request.user)
+    else:
+        cart_id = _cart_id(request)
+        cart_items = CartItem.objects.filter(cart__cart_id=cart_id)
+
+    return render(request, 'carts/checkout.html', {'cart_items': cart_items})
+
 
 '''def checkout(request):
     return render(request,'carts/checkout.html')'''
