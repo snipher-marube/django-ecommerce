b6067574f74221c8f98e83b096f2b94d8bb6b8e0
snipher <sniphermarube@gmail.com>
2024-08-21 16:47:43 +0300

    cart items

--- carts/views.py
+++ carts/views.py
@@ -56,7 +56,25 @@ def add_cart(request, product_id):
     messages.success(request, "The product was successfully added to the cart")
     return redirect('cart')
 
-def cart(request):
-    return render(request, 'carts/checkout.html')
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
 
+        for cart_item in cart_items:
+            total += cart_item.sub_total()
+            quantity += cart_item.quantity
 
+    except Cart.DoesNotExist:
+        cart_items = []  # No items in the cart, so initialize as an empty list
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items
+    }
+
+    return render(request, 'carts/checkout.html', context)
