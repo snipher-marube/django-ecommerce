85c8725c17da6325ff4b717fc8de8f91b50d3442
snipher <sniphermarube@gmail.com>
2024-08-23 08:47:44 +0300

    fix issue #41

--- DjangoEcommerce/urls.py
+++ DjangoEcommerce/urls.py
@@ -34,4 +34,6 @@ urlpatterns = [
     path('store/', include('products.urls')),
     path('orders/', include('orders.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
