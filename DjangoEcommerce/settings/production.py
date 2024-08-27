016918794e4cf22711201adb8aa04b38bed092f8
snipher <sniphermarube@gmail.com>
2024-08-27 06:59:07 +0300

    cloudinary issue on vercel

--- DjangoEcommerce/settings/production.py
+++ DjangoEcommerce/settings/production.py
@@ -42,17 +42,6 @@ SECURE_HSTS_PRELOAD = True
 DOMAIN = "https://django-ecommerces.vercel.app"
 CSRF_TRUSTED_ORIGINS = ['https://django-ecommerces.vercel.app']
 
-# This configuration block is setting up a cache using Redis for the Django project in a production environment.
-# The cache is used to store the results of expensive database queries, API calls, or other computationally expensive operations.
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': config('UPSTASH_REDIS_REST_URL'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
 
 # Static and media files settings for production
 STATIC_URL = '/static/'
