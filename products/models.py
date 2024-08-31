6032a0839c82a5ef51c98bb3ddb131601354ded5
snipher <sniphermarube@gmail.com>
2024-08-31 07:30:29 +0300

    cloudinary settings

--- products/models.py
+++ products/models.py
@@ -44,7 +44,7 @@ class Product(models.Model):
     discount_type = models.CharField(max_length=10, choices=DISCOUNT_TYPE_CHOICES, blank=True, null=True)
     discount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    image = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    image = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, null=True, validators=[validate_image_size])
     stock = models.PositiveBigIntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
